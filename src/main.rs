5c67e7a78222e9aac447a1fb3c11666bd0a4e339
masahitojp <randomstep@gmail.com>
2021-07-18 13:58:59 +0900

    chore: update version and fix README

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!"); // 数を当ててごらん
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line"); // 行の読み込みに失敗しました
+
+    println!("You guessed: {}", guess); // 次のように予想しました: {}
 }
